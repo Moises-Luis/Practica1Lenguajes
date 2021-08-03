fb7d82f4b0d7b0a181090d7f5fb06de0f9a9c82b
Moisés Luis <benjamin_luis_1999@hotmail.com>
2021-08-03 15:16:58 -0600

    Creación del marco principal

---
+++ src/main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class main {
+    public static void main(String args[]){
+        MarcoAnalizador miMarco = new MarcoAnalizador();
+        miMarco.setLocationRelativeTo(null);
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MarcoAnalizador extends JFrame{
+    public MarcoAnalizador(){
+        setSize(500,500);
+        LaminaAnalizador miLamina = new LaminaAnalizador();
+        add(miLamina);
+        setVisible(true);
+    }
+
+}
+class LaminaAnalizador extends JPanel{
+    public LaminaAnalizador(){
+
+    }
+}
