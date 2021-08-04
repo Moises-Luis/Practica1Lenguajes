a9f5561bdf6d3c336272978feb18de44347a530a
Moisés Luis <benjamin_luis_1999@hotmail.com>
2021-08-04 03:44:13 -0600

    Haciendo pruebas para el identificador de palabras

---
+++ src/Identificadores.java
@@ -0,0 +1,59 @@
+public enum Identificadores {
+        SIMBOLO1('[',"SIMBOLO"),
+        SIMBOLO2(']',"SIMBOLO"),
+        SIMBOLO3(';',"SIMBOLO"),
+        SIMBOLO4(',',"SIMBOLO"),
+        SIMBOLO5('.',"SIMBOLO"),
+        DIGITO1('0',"NÚMERO"),
+        DIGITO2('1',"NÚMERO"),
+        DIGITO3('2',"NÚMERO"),
+        DIGITO4('3',"NÚMERO"),
+        DIGITO5('4',"NÚMERO"),
+        DIGITO6('5',"NÚMERO"),
+        DIGITO7('6',"NÚMERO"),
+        DIGITO8('7',"NÚMERO"),
+        DIGITO9('8',"NÚMERO"),
+        DIGITO10('9',"NÚMERO"),
+        IDENTIFICADOR1('A',"IDENTIFICADOR"),
+        IDENTIFICADOR2('B',"IDENTIFICADOR"),
+        IDENTIFICADOR3('C',"IDENTIFICADOR"),
+        IDENTIFICADOR4('D',"IDENTIFICADOR"),
+        IDENTIFICADOR5('E',"IDENTIFICADOR"),
+        IDENTIFICADOR6('F',"IDENTIFICADOR"),
+        IDENTIFICADOR7('G',"IDENTIFICADOR"),
+        IDENTIFICADOR8('H',"IDENTIFICADOR"),
+        IDENTIFICADOR9('I',"IDENTIFICADOR"),
+        IDENTIFICADOR10('J',"IDENTIFICADOR"),
+        IDENTIFICADOR11('K',"IDENTIFICADOR"),
+        IDENTIFICADOR12('L',"IDENTIFICADOR"),
+        IDENTIFICADOR13('M',"IDENTIFICADOR"),
+        IDENTIFICADOR14('N',"IDENTIFICADOR"),
+        IDENTIFICADOR15('Ñ',"IDENTIFICADOR"),
+        IDENTIFICADOR16('O',"IDENTIFICADOR"),
+        IDENTIFICADOR17('P',"IDENTIFICADOR"),
+        IDENTIFICADOR18('Q',"IDENTIFICADOR"),
+        IDENTIFICADOR19('R',"IDENTIFICADOR"),
+        IDENTIFICADOR20('S',"IDENTIFICADOR"),
+        IDENTIFICADOR21('T',"IDENTIFICADOR"),
+        IDENTIFICADOR22('U',"IDENTIFICADOR"),
+        IDENTIFICADOR23('V',"IDENTIFICADOR"),
+        IDENTIFICADOR24('W',"IDENTIFICADOR"),
+        IDENTIFICADOR25('X',"IDENTIFICADOR"),
+        IDENTIFICADOR26('Y',"IDENTIFICADOR"),
+        IDENTIFICADOR27('Z',"IDENTIFICADOR");
+
+        private Identificadores(char caracter, String tipoIdentificador){
+            this.caracter = caracter;
+            this.tipoIdentificador = tipoIdentificador;
+        }
+
+        public char getCaracter(){
+            return caracter;
+        }
+        public String getTipoIdentificador(){
+            return tipoIdentificador;
+        }
+        char caracter;
+        String tipoIdentificador;
+
+}
--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class main {
     public static void main(String args[]){
@@ -35,6 +38,7 @@ class LaminaAnalizador extends JPanel{
         laminaSuperior.add(laminaTexto);
 
         boton = new JButton("Verificar");
+        boton.addActionListener(new Oyente());
         laminaSuperior.add(boton);
 
         areaPalabrasListadas = new JTextArea(9,22);
@@ -42,36 +46,125 @@ class LaminaAnalizador extends JPanel{
         laminaPalabrasListadas = new JScrollPane(areaPalabrasListadas);
         laminaCentral.add(laminaPalabrasListadas);
     }
+
+    String cadenaRestante;
+    String cadenaCortada;
+
+    public void obtenerPalabra(String cadena){
+
+        int posicion = 0;
+        int tamañoCadena = cadena.length();
+
+        for (int i = 0; i<cadena.length(); i++){
+            posicion = cadena.indexOf(" ");
+            if (posicion != -1){
+                cadenaCortada = cadena.substring(0,posicion);
+                cadenaRestante = cadena.substring(posicion, tamañoCadena);
+                break;
+            }else if (posicion == -1){
+                cadenaCortada = cadena.substring(0, cadena.length());
+                cadenaRestante = "";
+            }
+        }
+    }
+    String palabra;
+    String palabraCompleta;
+
+    public String getUnCaracterDeUnaPalabra(){
+        String aux, caracter = "";
+        aux = palabra;
+        System.out.println(aux.length());
+        if (aux.length() > 0)
+            caracter = aux.substring(0,1);
+        if (palabra.length()>0)
+            palabra = palabra.substring(1, palabra.length());
+            //System.out.println("Palabra restante:"+palabra);
+           // System.out.println("Caracter obtenido:"+caracter.toUpperCase());
+            return caracter;
+    }
+    public boolean getComparacionDelCaracter(int posFinal, String lexema){
+        String numEnum, nomEnumAux2;
+        String caracter = getUnCaracterDeUnaPalabra().toUpperCase();
+
+        for (int i = 1; i <= posFinal; i++){
+            numEnum = String.valueOf(i);
+            nomEnumAux2 = lexema+numEnum;
+
+            Identificadores id = Enum.valueOf(Identificadores.class, nomEnumAux2);
+            //System.out.println(getUnCaracterDeUnaPalabra() +"========"+id.getCaracter());
+
+            if (caracter.equals(String.valueOf(id.getCaracter()))){
+                System.out.println("Y el caracter es: "+id.getCaracter());
+                System.out.println("Y el tipo de Identificador es: "+id.getTipoIdentificador());
+                return true;
+
+            }
+        }
+        return false;
+    }
+    public void leer_caracter_a_caracter(){
+        boolean seEncontro = false;
+        //colocar el booleano para el caracter que sea incorrecto :V
+
+        getComparacionDelCaracter(5,"SIMBOLO");
+        getComparacionDelCaracter(10,"DIGITO");
+        getComparacionDelCaracter(27,"IDENTIFICADOR");
+
+    }
+
+    public void identificarPalabra(){
+        int cantCaracteres = palabra.length();
+        int primerCaracterId, finalCaracterId;
+//        char caracter;
+//        boolean isIdentificador;
+        boolean primerCaracterEncontrado;
+        String palabraId = "";
+        int iterador = 0;
+        for (int i = 0; i < cantCaracteres; i++) {
+            primerCaracterEncontrado = getComparacionDelCaracter(27,"IDENTIFICADOR");
+           // System.out.println(primerCaracterEncontrado);
+
+            if (primerCaracterEncontrado == true){
+                if (iterador == 0)
+                primerCaracterId = palabraCompleta.length()-(palabra.length()+1);
+                else
+                iterador++;
+
+            }else if (primerCaracterEncontrado == false){
+                palabraId = palabraCompleta.substring(0,iterador);
+                iterador = 0;
+                System.out.println(palabraId+" 'Es un IDENTIFICADOR' ");
+            }
+
+        }
+    }
+    public void identificarNumeroDecimal(){}
+    public void identifiicarCaracter(){}
+
+    private class Oyente implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+//            palabra = areaTexto.getText();
+//            leer_caracter_a_caracter();
+            palabra = areaTexto.getText();
+            palabraCompleta = areaTexto.getText();
+            identificarPalabra();
+
+            /*
+            String aux = areaTexto.getText().replaceAll("\n"," ");
+
+            obtenerPalabra(aux.trim());
+            System.out.println(cadenaCortada);
+            obtenerPalabra(cadenaRestante.trim());
+            System.out.println(cadenaCortada);
+            obtenerPalabra(cadenaRestante.trim());
+            System.out.println(cadenaCortada);
+            obtenerPalabra(cadenaRestante.trim());
+            System.out.println(cadenaCortada);
+
+             */
+        }
+    }
 }
 
-enum Identificadores{
-    SIMBOLO1('[',"SIMBOLO"),
-    SIMBOLO2(']',"SIMBOLO"),
-    SIMBOLO3(';',"SIMBOLO"),
-    SIMBOLO4(',',"SIMBOLO"),
-    SIMBOLO5('.',"SIMBOLO"),
-    DIGITO('0',"NÚMERO"),
-    DIGITO1('1',"NÚMERO"),
-    DIGITO2('2',"NÚMERO"),
-    DIGITO3('3',"NÚMERO"),
-    DIGITO4('4',"NÚMERO"),
-    DIGITO5('5',"NÚMERO"),
-    DIGITO6('6',"NÚMERO"),
-    DIGITO7('7',"NÚMERO"),
-    DIGITO8('8',"NÚMERO"),
-    DIGITO9('9',"NÚMERO");
-
-    private Identificadores(char caracter, String tipoIdentificador){
-        this.caracter = caracter;
-        this.tipoIdentificador = tipoIdentificador;
-    }
-
-    public char getCaracter(){
-        return caracter;
-    }
-    public String getTipoIdentificador(){
-        return tipoIdentificador;
-    }
-    char caracter;
-    String tipoIdentificador;
-}
