0af37d260450225778b1453f5402ba17476e2652
Moisés Luis <benjamin_luis_1999@hotmail.com>
2021-08-04 18:37:21 -0600

    varios cambios :V

--- src/main.java
+++ src/main.java
@@ -67,13 +67,39 @@ class LaminaAnalizador extends JPanel{
             }
         }
     }
+    public int obtenerNumPalabras(String cadena){
+        String auxCadena, aux2Cadena;
+        int posicion = 0;
+        int tamañoCadena = cadena.length();
+        int contador = 0;
+
+
+        posicion = cadena.indexOf(" ");
+        contador++;
+        auxCadena = cadena;
+        auxCadena = cadena.substring(posicion,cadena.length());
+        auxCadena.trim();
+
+        while (posicion != -1) {
+            System.out.println(auxCadena);
+            auxCadena = auxCadena.trim();
+            posicion = auxCadena.indexOf(" ");
+            if (posicion == -1)break;
+            contador++;
+            aux2Cadena = auxCadena.substring(posicion, auxCadena.length());
+            System.out.println(aux2Cadena);
+            auxCadena = aux2Cadena;
+        }
+        return contador+1;
+    }
+
     String palabra;
     String palabraCompleta;
 
     public String getUnCaracterDeUnaPalabra(){
         String aux, caracter = "";
         aux = palabra;
-        System.out.println(aux.length());
+        //System.out.println(aux.length());
         if (aux.length() > 0)
             caracter = aux.substring(0,1);
         if (palabra.length()>0)
@@ -82,9 +108,8 @@ class LaminaAnalizador extends JPanel{
            // System.out.println("Caracter obtenido:"+caracter.toUpperCase());
             return caracter;
     }
-    public boolean getComparacionDelCaracter(int posFinal, String lexema){
+    public Identificadores getComparacionDelCaracter(int posFinal, String lexema, String caracter){
         String numEnum, nomEnumAux2;
-        String caracter = getUnCaracterDeUnaPalabra().toUpperCase();
 
         for (int i = 1; i <= posFinal; i++){
             numEnum = String.valueOf(i);
@@ -94,62 +119,168 @@ class LaminaAnalizador extends JPanel{
             //System.out.println(getUnCaracterDeUnaPalabra() +"========"+id.getCaracter());
 
             if (caracter.equals(String.valueOf(id.getCaracter()))){
-                System.out.println("Y el caracter es: "+id.getCaracter());
-                System.out.println("Y el tipo de Identificador es: "+id.getTipoIdentificador());
-                return true;
+//                System.out.println("el caracter es: "+id.getCaracter());
+//                System.out.println("y el tipo de Identificador es: "+id.getTipoIdentificador());
+                return id;
 
             }
         }
-        return false;
-    }
-    public void leer_caracter_a_caracter(){
-        boolean seEncontro = false;
-        //colocar el booleano para el caracter que sea incorrecto :V
-
-        getComparacionDelCaracter(5,"SIMBOLO");
-        getComparacionDelCaracter(10,"DIGITO");
-        getComparacionDelCaracter(27,"IDENTIFICADOR");
-
+        return null;
     }
 
-    public void identificarPalabra(){
+    public void getPalabraIdentificada(){
         int cantCaracteres = palabra.length();
-        int primerCaracterId, finalCaracterId;
-//        char caracter;
-//        boolean isIdentificador;
-        boolean primerCaracterEncontrado;
-        String palabraId = "";
+        Identificadores identificadorAnterior = null, identificador;
+        String cadenaCaracteres = "";
         int iterador = 0;
+        boolean isId = false, isNumEntero = false,isNumDecimal = false, isSimbolo = false, isError = false;
+
+        boolean primerDigitoEsNumero = false, puntoDespNumero=false;
+        boolean yaEntro = false;
         for (int i = 0; i < cantCaracteres; i++) {
-            primerCaracterEncontrado = getComparacionDelCaracter(27,"IDENTIFICADOR");
-           // System.out.println(primerCaracterEncontrado);
+            //
+            String caracter = getUnCaracterDeUnaPalabra().toUpperCase();
 
-            if (primerCaracterEncontrado == true){
-                if (iterador == 0)
-                primerCaracterId = palabraCompleta.length()-(palabra.length()+1);
-                else
-                iterador++;
+            identificador = getComparacionDelCaracter(27,"IDENTIFICADOR", caracter);
 
-            }else if (primerCaracterEncontrado == false){
-                palabraId = palabraCompleta.substring(0,iterador);
-                iterador = 0;
-                System.out.println(palabraId+" 'Es un IDENTIFICADOR' ");
+            if (identificador == null){
+                identificador = getComparacionDelCaracter(10, "DIGITO", caracter);
+                System.out.println("Es un digito");
+            }
+            if (identificador == null) {
+                identificador = getComparacionDelCaracter(5, "SIMBOLO", caracter);
             }
 
+                if (identificador != null && iterador == 0){
+                    cadenaCaracteres += identificador.getCaracter();
+                    identificadorAnterior = identificador;
+                    iterador ++;
+                    System.out.println("Si entra al primer if :V");
+                    if (identificador.getTipoIdentificador() == "NÚMERO"){
+                        primerDigitoEsNumero = true;
+                        System.out.println("El primer digito es un número");
+                    }
+
+                }else if (identificadorAnterior.getTipoIdentificador() == identificador.getTipoIdentificador()){
+                    cadenaCaracteres += identificador.getCaracter();
+
+                    /*
+                    if (primerDigitoEsNumero==true && String.valueOf(identificador.getCaracter())== ".") {
+                        puntoDespNumero = true;
+                    }
+                    */
+
+                }else{
+                    String expresionCompleta="", nombreDelIdentificador="";
+
+                    if (primerDigitoEsNumero==false && identificadorAnterior.getTipoIdentificador()=="IDENTIFICADOR" && identificador.getTipoIdentificador()=="NÚMERO"){
+                        //expresionCompleta = "Identificador:   "+cadenaCaracteres+"\n";
+                        cadenaCaracteres += identificador.getCaracter();
+                        nombreDelIdentificador = "Identificador";
+                        yaEntro=true;
+
+                    }else if (identificador.getTipoIdentificador()=="SIMBOLO" && primerDigitoEsNumero==false){
+                        expresionCompleta = "Simbolo/Caracter: "+cadenaCaracteres+"\n";
+                        areaPalabrasListadas.append(expresionCompleta);
+                        cadenaCaracteres = "";
+                        cadenaCaracteres += identificador.getCaracter();
+                        identificadorAnterior = identificador;
+                        puntoDespNumero = false;
+                        primerDigitoEsNumero = false;
+
+                    }else if (primerDigitoEsNumero == true || identificadorAnterior.getTipoIdentificador()=="SIMBOLO" && identificador.getTipoIdentificador()=="NÚMERO"){
+                        nombreDelIdentificador = "Número Entero: ";
+                        cadenaCaracteres += identificador.getCaracter();
+                        System.out.println("SIIIIII ENTROOOOOOO");
+                        yaEntro = true;
+
+                    }else if (primerDigitoEsNumero == true && String.valueOf(identificadorAnterior.getCaracter()) == "." && identificador.getTipoIdentificador()=="NÚMERO"){
+                        nombreDelIdentificador = "Número decimal: ";
+                        cadenaCaracteres += identificador.getCaracter();
+                        yaEntro = true;
+
+                    }else if (primerDigitoEsNumero == true && String.valueOf(identificadorAnterior.getCaracter()) == "." && identificador.getTipoIdentificador()!= "NÚMERO"){
+                        expresionCompleta = "ERROR:   "+cadenaCaracteres+"\n";
+                        areaPalabrasListadas.append(expresionCompleta);
+                    }else if(yaEntro==true){
+                        System.out.println("TTTTTTTTAMBIENN ETNRROOOOO");
+                        expresionCompleta = nombreDelIdentificador +cadenaCaracteres+"\n";
+                        areaPalabrasListadas.append(expresionCompleta);
+                        cadenaCaracteres = "";
+                        cadenaCaracteres += identificador.getCaracter();
+                        identificadorAnterior = identificador;
+                        puntoDespNumero = false;
+                        primerDigitoEsNumero = false;
+                        yaEntro= false;
+                    }
+                }
+        }
+        if (identificadorAnterior != null) {
+            String expresionCompleta = ">>>>>>>»»»" + ":   " + cadenaCaracteres + "\n";
+            areaPalabrasListadas.append(expresionCompleta);
         }
     }
-    public void identificarNumeroDecimal(){}
+
+
+    public void identificarNumeroDecimal(){
+
+    }
     public void identifiicarCaracter(){}
 
     private class Oyente implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-//            palabra = areaTexto.getText();
-//            leer_caracter_a_caracter();
-            palabra = areaTexto.getText();
-            palabraCompleta = areaTexto.getText();
-            identificarPalabra();
+            String auxCadena="", aux2CadenaResto = "", cadena;
+            int posicion = 0;
+            cadena = areaTexto.getText().replaceAll("\n", " ");
+            do {
+                cadena = cadena.trim();
+                posicion = cadena.indexOf(" ");
+                if (posicion == -1)break;
+                auxCadena = cadena.substring(0, posicion);
+                System.out.println(auxCadena); //////////////////////////////////////////
+                palabra = auxCadena.trim();
+                getPalabraIdentificada();
+
+                aux2CadenaResto = cadena.substring(posicion, cadena.length());
+                cadena = aux2CadenaResto.trim();
+
+                auxCadena.trim();
+
+            }while (posicion != -1);
+            System.out.println(aux2CadenaResto.trim());///////////////////
+            palabra = aux2CadenaResto.trim();
+            getPalabraIdentificada();
+
+            /*
+            while (posicion != -1) {
+                System.out.println(auxCadena);
+                auxCadena = auxCadena.trim();
+                posicion = auxCadena.indexOf(" ");
+                if (posicion == -1)break;
+                aux2CadenaResto = auxCadena.substring(posicion, auxCadena.length());
+                System.out.println(aux2CadenaResto);
+                auxCadena = aux2CadenaResto;
+            }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            String aux = areaTexto.getText().replaceAll("\n", "");
+//            palabra = aux.trim();
+//            palabraCompleta = areaTexto.getText();
+//            System.out.println(getPalabraIdentificada());
 
             /*
             String aux = areaTexto.getText().replaceAll("\n"," ");
@@ -168,3 +299,9 @@ class LaminaAnalizador extends JPanel{
     }
 }
 
+/*
+|| primerDigitoEsNumero==true && String.valueOf(identificador.getCaracter())=="."
+                || puntoDespNumero== true && identificador.getTipoIdentificador() == "NÚMERO" || primerDigitoEsNumero==false && identificadorAnterior.getTipoIdentificador()=="IDENTIFICADOR"
+                 || primerDigitoEsNumero==false && identificador.getTipoIdentificador()=="IDENTIFICADOR" || primerDigitoEsNumero == true && identificador.getTipoIdentificador()=="IDENTIFICADOR"
+
+ */
